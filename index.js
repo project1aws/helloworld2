c886fd895bfe59928e5df04959e763ee79123ecf
project1aws <33067752+project1aws@users.noreply.github.com>
2017-10-30 14:06:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2')
+  response.send('changed to do test bla bla bla ')
 })
 
 app.listen(app.get('port'), function() {
